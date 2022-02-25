c1b8f0537e3cb2cd8ae3c9f3232283493382bc66
himuy3 <gabriel.severo09@hotmail.com>
2022-02-25 16:36:24 -0300

    explication

---
+++ repetition_structures_exercises/basic.js
@@ -0,0 +1,41 @@
+for (var index = 0; index < numero; index++) {
+                                    // index--
+}
+
+
+
+
+
+while (true) {
+  
+}
+
+
+//5! = 1 * 2* 3* 4* 5 = 120
+var fatorial = 6
+var resultado = fatorial
+
+for (var index = 1; index < fatorial; index++) {
+    resultado = resultado * index
+}
+
+console.log(resultado)
+//primeiro loop
+//1 < 5
+//resultado(5) = resultado (5) * 1
+
+//segundo loop
+//2 < 5
+// resultado (5) = resultado (5) * 2
+
+//terceiro loop
+//3 < 5
+//resultado(10) = resultado (10) * 3
+
+//quarto loop
+//4 < 5
+//resultado(30) = resultado(30) * 4
+
+//quinto loop
+// 5 < 5 nao
+//sai do loop
