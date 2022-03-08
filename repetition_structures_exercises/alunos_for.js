037e2e1b9e2b430856c7434fa96efdff701fb8fe
himuy3 <gabriel.severo09@hotmail.com>
2022-03-08 17:58:56 -0300

    repetition

---
+++ repetition_structures_exercises/alunos_for.js
@@ -0,0 +1,13 @@
+var prompt = require("prompt-sync")();
+var alunos = 3;
+var resultado_final = 0;
+
+for (var index = 1; index <= 3; index++) {
+  var prova = parseInt(prompt("nota da prova"));
+  var trabalho = parseInt(prompt("nota do trabalho"));
+  var tarefas = parseInt(prompt("nota das tarefas"));
+  var notas = (prova + trabalho + tarefas) / 3;
+  var resultado_final = notas + resultado_final;
+  console.log(resultado_final);
+}
+  console.log(resultado_final / 3)
