5b196de2e68ea5c6f249cc429a8e21b49854859e
himuy3 <gabriel.severo09@hotmail.com>
2022-03-23 18:56:23 -0300

    palindromo

--- arrays/numerorepetido.js
+++ arrays/numerorepetido.js
@@ -1,13 +1,13 @@
-var teste = [2, 2, 4, 1, 5, 4, 3, 4, 2, 3]
+var teste = [2, 2, 4, 1, 5, 4, 3, 4, 2, 3];
 
-for (var i = 0; i < teste.length; i++){
-    var teste1 = teste[i] 
+for (var i = 0; i < teste.length; i++) {
+    var teste1 = teste[i];
     for (var j = 0; j < teste.length; j++) {
-       var teste2 = teste[j];
-        if ( i !== j && teste1 === teste2 ) {
-            console.log(`${i} é igual a ${j}`)
-        } else{
-            console.log(`${i} não é igual ${j}`)
-        } 
-    } 
-}  
+        var teste2 = teste[j];
+        if (i !== j && teste1 === teste2) {
+            console.log(`${i} é igual a ${j}`);
+        } else {
+            console.log(`${i} não é igual ${j}`);
+        }
+    }
+}
