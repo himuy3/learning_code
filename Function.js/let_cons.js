fcc6ea759a9a5d79b557239275a937ab7430d875
himuy3 <gabriel.severo09@hotmail.com>
2022-03-25 15:09:26 -0300

    function

---
+++ Function.js/let_cons.js
@@ -0,0 +1,25 @@
+var palindromo = "agua";
+var assist = palindromo.length - 1;
+var flag = false;
+
+for (var i = 0; i < palindromo.length; i++) {
+    var pal1 = palindromo[i];
+    var assist2 = assist - i 
+    for (var j = assist; j >= 0; j--) {
+        var pal2 = palindromo[j];
+        if (assist2 == j && pal1 !== pal2) {
+            flag = true;
+        }
+    }
+}
+if (flag) {
+    console.log("n é palindromo");
+} else {
+    console.log("é palindromo");
+}
+
+
+
+function name(params) {
+    
+}
